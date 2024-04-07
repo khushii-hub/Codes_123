099bb79f00159f2627fcfc1809ae99eba03f7140
khushii-hub <72705845+khushii-hub@users.noreply.github.com>
2024-04-07 18:35:35 +0530

    Add files via upload

---
+++ connect php server.php
@@ -0,0 +1,13 @@
+
+<?php  
+$host = 'localhost:3306';  
+$user = 'root';  
+$pass = '';  
+$conn = mysqli_connect($host, $user, $pass);  
+if(! $conn )  
+{  
+  die('Could not connect: ' . mysqli_error());  
+}  
+echo 'Connected successfully';  
+mysqli_close($conn);  
+?>  
